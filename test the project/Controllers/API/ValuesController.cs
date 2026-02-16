92b1c9b5e42b502361d7a1d09d535869a4a2c013
mahmoud Abotaleb <mahmoudabotaleb364@gmail.com>
2026-02-16 13:08:59 +0200

    add value controller add delete

---
+++ test the project/Controllers/API/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_the_project.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
